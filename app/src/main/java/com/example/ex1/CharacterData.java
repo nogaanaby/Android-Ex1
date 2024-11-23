2886f227df9d9aad99bf34b5a3240204a485f06d
Mor Boneh <mormor318@gmail.com>
2024-11-23 17:12:30 +0200

    changed Zog to Mor

--- app/src/main/java/com/example/ex1/CharacterData.java
+++ app/src/main/java/com/example/ex1/CharacterData.java
@@ -9,7 +9,7 @@ public class CharacterData {
                 new Character("Princess Tiabeanie", "bean@example.com", "123-456-7890", "Getting Stone", R.drawable.bean),
                 new Character("Elfo", "elfo@example.com", "987-654-3210", "bouncing", R.drawable.elfo),
                 new Character("Luci", "luci@example.com", "987-654-3210", "making fun of people", R.drawable.luci),
-                new Character("Zog", "zog@example.com", "987-654-3210", "I don't have time for hobbies", R.drawable.zog),
+                new Character("Mor", "zog@example.com", "987-654-3210", "I don't have time for hobbies", R.drawable.zog),
                 new Character("Mop Girl", "mop_girl@example.com", "987-654-3210", "writing novels", R.drawable.mop_girl)
         );
     }
